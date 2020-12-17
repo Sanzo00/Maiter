bd0d5b385acfe0c46dbcb5041d97cd29506c5e78
Sanzona <arrangeman@163.com>
2020-12-17 13:26:15 +0000

    update

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
+echo "make"
 make
+
+echo "cp bin/release/examples/example-dsm ."
 cp bin/release/examples/example-dsm .
+
+echo "mkdir input"
+mkdir input
+
+echo "mkdir result"
+mkdir result
+
+echo "tail -n 5105039 doc/web-Google.txt > input/part0"
+tail -n 5105039 doc/web-Google.txt > input/part0
